4e0a0038411a201689bd32f5801f4828225ed502
Tapas05-ui <tapast2004@gmail.com>
2026-05-17 10:00:11 +0530

    error fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://pathfinder-6u1j.onrender.com/api",
   withCredentials: true,
 });
 
